0f66187092b91c84c6bc5248982de99df45edd98
matt-codeslo <matt@codeslo.com>
2017-02-18 22:39:40 -0800

    query now filters out india. Also, date is dynamic

--- main.js
+++ main.js
@@ -28,8 +28,12 @@ $(document).ready(function () {
 	});
 	// function to load videos
 	function loadVideo() {
+		var d = new Date();
+		var lastYear = d.getFullYear() - 1;
+		var month = d.getMonth() +1;
+		var day = d.getDate();
 		var myKey = "AIzaSyAr0fnc8B-t96isVPUByudWNPFDKJIugoc";
-		var request = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=viewCount&publishedAfter=2017-01-01T00%3A00%3A00Z&q=teaser%7Ctrailer&type=video&videoCaption=any&videoCategoryId=24&videoEmbeddable=true&key=' + myKey;
+		var request = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=viewCount&publishedAfter='+lastYear+'-'+month+'-'+day+'T00%3A00%3A00Z&q=teaser|trailer -india|game&type=video&videoCaption=any&relevanceLanguage=en&videoCategoryId=24&videoEmbeddable=true&key=' + myKey;
 		$.ajax({
 			url: request,
 			success: function (data) {
